0456cc945e0e3c1bff09878d03ca6e6d437cb869
deq <himanshu.1630@gmail.com>
2025-06-02 11:51:11 +0530

    Simple calculator

---
+++ src/sept/ex_16092024/Lab033_Unary_Operator.java
@@ -0,0 +1,9 @@
+package sept.ex_16092024;
+
+public class Lab033_Unary_Operator {
+    public static void main(String[] args) {
+        //Unary operator
+        int a = +65;  // +--> Unary operator
+        int b =  -1; // - unary operator
+    }
+}
